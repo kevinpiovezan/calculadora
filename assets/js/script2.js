cc10151b8b098a6fd4e5aa1081babfcbfc0940a0
Kevin Piovezan <kevinpiovezan@gmail.com>
2021-08-06 05:36:14 -0300

    Adicionando funcionalidade conversao

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,68 +1,22 @@
-(function () {
-    function criaCalculadora() {
-            const display = document.querySelector('.display');
-
-             function iniciaCalculadora() {
-                clickBotoes();
-                pressionaEnter();
-            }
-            
-            function clearDisplay() {
-                display.value = '';
-            }
-            
-            function apagaUm() {
-                display.value = display.value.slice(0, -1);
-            }
-
-            function pressionaEnter() {
-                document.addEventListener('keypress', (e) => {
-                    if (e.keyCode === 13) {
-                        return realizaConta();
-                    }
-                });
-            }
-            
-            function realizaConta() {
-                let conta = display.value
-                try {
-                    conta = eval(conta);
-                    if (!conta) {
-                        alert('Conta inválida!')
-                        return
-                    }
-                    display.value = String(conta);
-                } catch (e) {
-                    alert('Conta inválida!')
-                    return
-                }
-            }
-
-            function clickBotoes() {
-                document.addEventListener('click', e => {
-                    const el = e.target;
-                    
-                    if (el.classList.contains('btn-num')) {
-                        btnParaDisplay(el.innerText);
-                    }
-                    if (el.classList.contains('btn-clear')) {
-                        clearDisplay();
-                    }
-                    if (el.classList.contains('btn-del')) {
-                        apagaUm();
-                    }
-                    if (el.classList.contains('btn-eq')) {
-                        realizaConta();
-                    }
-                    
-                })
-            }
-            function btnParaDisplay(valor) {
-                display.value += valor;
-            }
-            iniciaCalculadora();
+(function() {
+    function converte() {
+        const input = Number(document.querySelector('.input').value);
+        let output = document.querySelector('.output');
+        let select = document.querySelector('.input-select');
+        let value = select.options[select.selectedIndex].value;
+        const options = document.getElementsByTagName('option');
+        if(typeof input !== 'number'){
+            alert('Digite um número válido');
         }
+        output.value = input * value + 'ml/g';
+    }
+    addEventListener('click', e => {
+        const el = e.target;
+        if(el.classList.contains("btn-eq")) converte();
+    });
+    addEventListener('keypress', e => {
+        if(e.keyCode === 13) converte();
+    })
     
-    criaCalculadora();
 
 })();
